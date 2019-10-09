1aaa2e95a881ab6d7d429fbfa8831725d017920c
ddhoogduin <d.d.hoogduin@lumc.nl>
2019-10-09 17:32:34 +0200

    change requirements and dir finder

--- shell/directoryFinder.sh
+++ shell/directoryFinder.sh
@@ -8,8 +8,10 @@ do
     if [[ ${cleared_path} =~ "@home@" ]];
     then
         new_path="$(echo "$HOME")$( echo ${cleared_path} | sed 's#@home@##g')"
+    else
+        new_path=${cleared_path}
     fi
-
+    echo ${new_path}
     if [[ -d ${new_path} ]]
     then
         if [[ -d "${new_path}/config" && -f "${new_path}/public/admin/config.js" ]]
